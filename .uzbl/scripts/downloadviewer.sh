6a8eabae20fb945643447dda3544426c8b05f5e8
Urbain Vaes <urbain@vaes.uk>
2016-03-04 00:12:48 +0000

    Remove uzbl

--- .uzbl/scripts/downloadviewer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-case "$1" in
-*.pdf*) zathura "$1" & ;;
-*.jpg*|*.png*|*.jpeg*) feh "$1" & ;;
-*.txt*|*README*|*.pl*|*.sh*|*.py*|*.hs*|*.hl*) urxvt -e vim "$1" & ;;
-*.mov*|*.avi*|*.mpeg*|*.mpg*|*.flv*|*.wmv*|*.mp4*) mplayer "$1" & ;;
-esac
