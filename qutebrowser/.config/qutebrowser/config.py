7b9d76d5db679c6c463077551a4a9b66f89c4db5
Urbain Vaes <urbain@vaes.uk>
2018-01-22 18:35:18 +0100

    [qutebrowser] Add script for passwords

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,8 +4,9 @@
 
 c.downloads.location.prompt = False
 c.editor.command = ['urxvt', '-e', 'nvim', '-f', '{}']
+c.fonts.hints = 'bold 12pt monospace'
 c.hints.chars = 'aoeuidhtns'
-# c.hints.uppercase = True
+c.hints.uppercase = True
 c.tabs.favicons.show = False
 c.tabs.show = 'multiple'
 c.tabs.tabs_are_windows = True
@@ -14,11 +15,11 @@ c.url.default_page = c.url.start_pages[0]
 
 # Bindings for normal mode
 config.bind(",b", 'spawn --userscript explorer -b')
-config.bind(",h", 'spawn --userscript explorer -h')
 config.bind(",p", 'spawn --userscript explorer')
 config.bind(",t", 'spawn --userscript explorer -t')
 config.bind(",v", 'spawn -d mpv {url}')
 config.bind(";v", 'hint links spawn -d mpv {hint-url}')
+config.bind(",x", 'spawn --userscript user-password')
 config.bind("]d", 'set downloads.location.prompt True')
 config.bind("[d", 'set downloads.location.prompt False')
 config.bind(",do", 'download-open')
@@ -28,14 +29,15 @@ config.bind("f", 'hint all current')
 
 # Bindings for insert mode
 config.bind("<Ctrl-A>", 'fake-key <Home>', mode='insert')
+config.bind("<Ctrl-E>", 'fake-key <End>', mode='insert')
 config.bind("<Ctrl-H>", 'fake-key <Backspace>', mode='insert')
 config.bind("<Ctrl-W>", 'fake-key <Ctrl-Backspace>', mode='insert')
-config.bind("<Ctrl-E>", 'fake-key <End>', mode='insert')
 config.bind("<Ctrl-F>", 'fake-key <Right>', mode='insert')
 config.bind("<Ctrl-B>", 'fake-key <Left>', mode='insert')
 config.bind("<Ctrl-N>", 'fake-key <Down>', mode='insert')
 config.bind("<Ctrl-P>", 'fake-key <Up>', mode='insert')
-config.bind("<Ctrl-i>", 'open-editor', mode='insert')
+config.bind("<Ctrl-I>", 'open-editor', mode='insert')
+config.bind("<Ctrl-X>", 'spawn --userscript user-password', mode='insert')
 
 # Bindings for command mode
 config.bind('<Ctrl-W>', 'fake-key -g <Ctrl-Backspace>', mode='command')
@@ -45,11 +47,12 @@ config.bind('<Ctrl-N>', 'completion-item-focus next', mode='command')
 # Search engines
 c.url.searchengines = {
     'DEFAULT': 'https://startpage.com/do/search?query={}',
-    'yt': 'https://www.youtube.com/results?search_query={}',
-    'ddg': 'https://duckduckgo.com/?q={}',
-    'google': 'https://www.google.com/search?hl=en&q={}',
     'aw': 'https://wiki.archlinux.org/index.php?search={}',
+    'ddg': 'https://duckduckgo.com/?q={}',
     'dic': 'https://dictionary.cambridge.org/dictionary/english/{}',
-    'tr': 'https://translate.google.com/#en/fr/{}',
+    'google': 'https://www.google.com/search?hl=en&q={}',
+    'hn': 'https://hn.algolia.com/?query={}',
     'scholar': 'https://scholar.google.com/scholar?q={}',
+    'tr': 'https://translate.google.com/#en/fr/{}',
+    'yt': 'https://www.youtube.com/results?search_query={}',
 }
