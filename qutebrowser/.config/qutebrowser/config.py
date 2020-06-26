1b0a177be70c9a8bdef2affc4c318c9afdcab9d3
Urbain Vaes <urbain@vaes.uk>
2020-06-26 12:38:14 +0200

    [qutebrowser] Block youtube adds

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,6 +2,19 @@
 #   qute://help/configuring.html
 #   qute://help/settings.html
 
+from qutebrowser.api import interceptor
+
+
+def filter_yt(info: interceptor.Request):
+    url = info.request_url
+    if (url.host() == 'www.youtube.com'
+            and url.path() == '/get_video_info'
+            and '&adformat=' in url.query()):
+        info.block()
+
+
+interceptor.register(filter_yt)
+
 c.content.images = True
 c.downloads.location.prompt = False
 c.editor.command = ['urxvt', '-e', 'nvim', '-f', '{}']
